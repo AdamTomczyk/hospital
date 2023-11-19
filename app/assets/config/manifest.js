57c1d033409c010f505990987644e7bc3343d257
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2023-11-19 10:27:25 +0100

    setup done

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+// app/assets/config/manifest.js
+//= link popper.js
+//= link bootstrap.min.js
